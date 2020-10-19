fdb775a269a82373305161a64bd313aa10339db6
Gonza Iglesias <gonns.iglesias@gmail.com>
2020-10-19 07:46:44 -0300

    Adding test and maven

--- src/test/java/com/tateti/JugarTatetiTest.java
+++ src/test/java/com/tateti/JugarTatetiTest.java
@@ -1,4 +1,21 @@
 package com.tateti;
 
+import org.junit.Test;
+
 public class JugarTatetiTest {
+
+    @Test
+    public  void weWithTwoHrzShouldWin(){
+        return ;
+    }
+
+    @Test
+    public  void userWithTwoHrzAndWeCouldWinWeShouldWin(){
+        return ;
+    }
+
+    @Test
+    public  void userWithTwoHrzAndWeNotWinningUserShouldBlocked(){
+        return ;
+    }
 }
